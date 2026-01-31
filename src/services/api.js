94785cbf8075b0c75520d5e85342f481af5e7a48
Yashi <sharmayashi270@gmail.com>
2026-01-31 21:37:55 +0530

    backend url added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://hrms-backend-py7z.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
